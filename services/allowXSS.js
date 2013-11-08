3cd482be8cfa91dae2836f55918a17cbbebe8af4
Jim McAndrew <jim@loc8.us>
2013-11-08 16:49:05 -0700

    Add service end points

---
+++ services/allowXSS.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+exports = module.exports = function(app) {
+    // Allow Cross Site Requests
+    var allowCrossDomain = function(req, res, next) {
+        var methods = 'GET,PUT,POST,DELETE,OPTIONS';
+        res.header('Access-Control-Allow-Credentials', true);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', methods);
+        res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        } else {
+            next();
+        }
+    };
+
+    app.use(allowCrossDomain);
+    //app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+};
